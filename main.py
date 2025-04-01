b732f3aeb207fa9df5206d8e326e3feebf94aa43
borasaygac <borasaygac14@gmail.com>
2025-04-01 14:54:24 +0200

    delta time added and fps limit to 60

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from constants import *
 def main():
 
     pygame.init() #init pygame
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #define the screen size
-    while True:
+    
+    while True: #infinite game loop
         
         for event in pygame.event.get(): # Check for closed window and quit game
             if event.type == pygame.QUIT:
@@ -13,6 +16,8 @@ def main():
             
         pygame.Surface.fill(screen, "black") 
         pygame.display.flip()
+        delta = clock.tick(60) # 60 fps limit
+        dt = delta / 1000 # delta time between frames in seconds
 
 if __name__ == "__main__":
     main()
