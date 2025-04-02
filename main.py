0e627163795a65e912f82d4bc6f187c2806d1a41
borasaygac <borasaygac14@gmail.com>
2025-04-02 15:32:26 +0200

    kill all colliding asteroids and bullets

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         for obj in drawable:
             obj.draw(screen)
